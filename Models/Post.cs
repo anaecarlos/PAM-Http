abd1a4f0e3640ed1ef92162f446c35cae006c7df
Ana e Carlos <anatevezvitoriarosseto@gmail.com>
2024-06-11 12:34:14 -0300

    Criação de base Http

---
+++ Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.OnlineId;
+
+namespace ExempleHttp.Models
+{
+    public class Post
+    {
+        //UserId
+        public int UserId { get; set; }
+        // Id
+        private int id;
+        public int Id { get => id; set => id = value; }  
+        //Title
+        public string Tittle { get; set; }
+        //Body
+        public string Body { get; set; }
+    }
+}
---
+++ Services/RestService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ExempleHttp.Models;
+using System.Text.Json;
+
+namespace ExempleHttp.Services
+{
+    public class RestService
+    {
+        private HttpClient client;
+        private Post post;
+        private List<Post> posts;
+        private JsonSerializerOptions _serializerOptions;
+
+        RestService()
+        {
+            client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+             };
+
+
+        }
+
+        public async Task<List<Post>> GetPostsAsync()
+        {
+            Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts"); 
+            HttpResponseMessage response = await client.GetAsync(uri);
+                if(response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    posts = JsonSerializer.Deserialize<List<Post>>(content, _serializerOptions);
+                }
+
+            return posts;
+        }
+    }
+}
